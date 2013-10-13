5e2466275f61239186b0686349c8f6ebdf20de64
Isaac Murchie <imurchie@gmail.com>
2013-10-13 15:05:20 -0700

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecordLabelApp::Application.config.secret_key_base = 'b5d220cb980c697f89e1dde61e1e7076503a4d5c59602493c5d381e64ce7ce214cec815412530339c2d393df6655aa22da7b7a7b57831e16f3d812200aac47a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RecordLabelApp::Application.config.session_store :cookie_store, key: '_record_label_app_session'
